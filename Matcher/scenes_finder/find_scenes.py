495d52718021ab8d58cd96e5438f9da73cc7dba2
HRAshton <12210721+HRAshton@users.noreply.github.com>
2024-12-29 22:31:10 +0100

    Simplify code

--- Matcher/scenes_finder/find_scenes.py
+++ Matcher/scenes_finder/find_scenes.py
@@ -93,19 +93,20 @@ def _get_endings(playlists_and_durations: List[tuple[M3U8, float]]) -> List[Inte
 
 
 def _combine_scenes(opening: Interval, ending: Interval, total_duration: float) -> Scenes:
-    new_opening = opening if not math.isnan(opening.start) else None
-    new_ending = ending if not math.isnan(ending.start) else None
+    new_opening = _valid_or_none(opening)
+    new_ending = _valid_or_none(ending)
 
+    # Calculate the scene after the ending or extend the ending.
     scene_after_ending = None
     if new_ending is not None:
-        if abs(total_duration - ending.end) > Config.scene_after_opening_threshold_secs:
-            scene_after_ending = Interval(ending.end, total_duration)
+        if total_duration - new_ending.end > Config.scene_after_opening_threshold_secs:
+            scene_after_ending = Interval(new_ending.end, total_duration)
         else:
-            new_ending = Interval(ending.start, total_duration)
+            new_ending = Interval(new_ending.start, total_duration)
 
-    return Scenes(_filter_scene(new_opening),
-                  _filter_scene(new_ending),
-                  _filter_scene(scene_after_ending))
+    return Scenes(new_opening,
+                  new_ending,
+                  _valid_or_none(scene_after_ending))
 
 
 def _fix_openings(openings: List[Interval],
@@ -115,8 +116,7 @@ def _fix_openings(openings: List[Interval],
     Fix openings by extending them to the beginning or prolonging them to the median duration.
     """
     fixed_openings: List[Interval] = []
-    zipped: List[tuple[Interval, float, tuple[M3U8, float]]] \
-        = list(zip(openings, truncated_durations, playlists_and_durations))
+    zipped = list(zip(openings, truncated_durations, playlists_and_durations))
     median_duration = median([opening.end - opening.start for opening, _, _ in zipped])
     for opening, duration, (_, total_duration) in zipped:
         if opening.start < Config.scene_after_opening_threshold_secs:
@@ -136,8 +136,7 @@ def _fix_endings(endings: List[Interval],
                  playlists_and_durations: List[tuple[M3U8, float]],
                  truncated_durations: List[float]) -> List[Interval]:
     fixed_endings: List[Interval] = []
-    zipped: List[tuple[Interval, float, tuple[M3U8, float]]] \
-        = list(zip(endings, truncated_durations, playlists_and_durations))
+    zipped = list(zip(endings, truncated_durations, playlists_and_durations))
     for ending, duration, (_, total_duration) in zipped:
         # Endings are truncated from the beginning, so we need to offset them.
         offset = total_duration - duration
@@ -147,9 +146,12 @@ def _fix_endings(endings: List[Interval],
     return fixed_endings
 
 
-def _filter_scene(scene: Interval | None) -> Interval | None:
+def _valid_or_none(scene: Interval | None) -> Interval | None:
     return (scene
-            if scene is not None and scene.end - scene.start >= Config.min_scene_length_secs
+            if (scene is not None
+                and not math.isnan(scene.start)
+                and not math.isnan(scene.end)
+                and scene.end - scene.start >= Config.min_scene_length_secs)
             else None)
 
 
