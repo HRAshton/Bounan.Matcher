9b2df3bf42a21c07e13650105c323808adadcaff
HRAshton <12210721+HRAshton@users.noreply.github.com>
2025-02-09 18:53:51 +0100

    Use decorator for config

--- Matcher/config/config.py
+++ Matcher/config/config.py
@@ -9,83 +9,107 @@ T = TypeVar("T")
 PARAMETER_NAME = os.environ.get('CONFIGURATION_PARAMETER_NAME')
 
 
+def _add_name(func):
+    def wrapper(self):
+        return func(self, func.__name__)
+
+    return wrapper
+
+
 class _Config:
-    _configuration: dict[str, str] | None = None
+    _configuration: dict[str, str] = {}
 
     def initialize_from_ssm(self) -> None:
         assert PARAMETER_NAME is not None, "CONFIGURATION_PARAMETER_NAME is not set."
         runtime_config_json = ssm_client.get_ssm_parameter(PARAMETER_NAME)
-        self._configuration = json.loads(runtime_config_json)
+        self.initialize_from_dict(json.loads(runtime_config_json))
 
     def initialize_from_dict(self, configuration: dict[str, str]) -> None:
-        self._configuration = configuration
+        self._configuration.clear()
+        self._configuration.update(configuration)
 
     def export(self) -> dict[str, str]:
         assert self._configuration is not None, "Configuration is not initialized."
         return dict(self._configuration)
 
     @property
-    def loan_api_token(self) -> str:
-        return self._get_value('loan_api_token')
+    @_add_name
+    def loan_api_token(self, name: str = "") -> str:
+        return self._get_value(name)
 
     @property
-    def log_group_name(self) -> str:
-        return self._get_value('log_group_name')
+    @_add_name
+    def log_group_name(self, name: str = "") -> str:
+        return self._get_value(name)
 
     @property
-    def log_level(self) -> str:
-        return self._get_value('log_level', 'INFO')
+    @_add_name
+    def log_level(self, name: str = "") -> str:
+        return self._get_value(name, 'INFO')
 
     @property
-    def episodes_to_match(self) -> int:
-        return int(self._get_value('episodes_to_match', 5))
+    @_add_name
+    def episodes_to_match(self, name: str = "") -> int:
+        """ Number of episodes to match cross each other. """
+        return int(self._get_value(name, 5))
 
     @property
-    def seconds_to_match(self) -> int:
-        return int(self._get_value('seconds_to_match', 6 * 60))
+    @_add_name
+    def seconds_to_match(self, name: str = "") -> int:
+        return int(self._get_value(name, 6 * 60))
 
     @property
-    def notification_queue_url(self) -> str:
-        return self._get_value('notification_queue_url')
+    @_add_name
+    def notification_queue_url(self, name: str = "") -> str:
+        return self._get_value(name)
 
     @property
-    def get_series_to_match_lambda_name(self) -> str:
-        return self._get_value('get_series_to_match_lambda_name')
+    @_add_name
+    def get_series_to_match_lambda_name(self, name: str = "") -> str:
+        return self._get_value(name)
 
     @property
-    def update_video_scenes_lambda_name(self) -> str:
-        return self._get_value('update_video_scenes_lambda_name')
+    @_add_name
+    def update_video_scenes_lambda_name(self, name: str = "") -> str:
+        return self._get_value(name)
 
     @property
-    def temp_dir(self) -> str:
-        return self._get_value('temp_dir', '/tmp')
+    @_add_name
+    def temp_dir(self, name: str = "") -> str:
+        return self._get_value(name, '/tmp')
 
     @property
-    def download_threads(self) -> int:
-        return int(self._get_value('download_threads', 12))
+    @_add_name
+    def download_threads(self, name: str = "") -> int:
+        return int(self._get_value(name, 12))
 
     @property
-    def download_max_retries_for_ts(self) -> int:
-        return int(self._get_value('download_max_retries_for_ts', 3))
+    @_add_name
+    def download_max_retries_for_ts(self, name: str = "") -> int:
+        return int(self._get_value(name, 3))
 
     @property
-    def scene_after_opening_threshold_secs(self) -> int:
-        return int(self._get_value('scene_after_opening_threshold_secs', 4))
+    @_add_name
+    def scene_after_opening_threshold_secs(self, name: str = "") -> int:
+        return int(self._get_value(name, 4))
 
     @property
-    def min_scene_length_secs(self) -> int:
-        return int(self._get_value('min_scene_length_secs', 20))
+    @_add_name
+    def min_scene_length_secs(self, name: str = "") -> int:
+        return int(self._get_value(name, 20))
 
     @property
-    def operating_log_rate_per_minute(self) -> int:
-        return int(self._get_value('operating_log_rate_per_minute', 1))
+    @_add_name
+    def operating_log_rate_per_minute(self, name: str = "") -> int:
+        return int(self._get_value(name, 1))
 
     @property
-    def batch_size(self) -> int:
+    @_add_name
+    def batch_size(self, name: str = "") -> int:
         # 20 is set to avoid rate limits on Publisher side.
         # It will automatically expand up to 10*2-1=19
         # if last batch contains less than 10 videos.
-        return int(self._get_value('batch_size', 10))
+        return int(self._get_value(name, 10))
 
     def _get_value(self, key: str, default: T | None = None) -> str:
         assert self._configuration is not None, "Configuration is not initialized."
