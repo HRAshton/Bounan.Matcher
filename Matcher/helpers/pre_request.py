e72209c9c4ed11b781b0b7ba4abebcc611a9764c
HRAshton <12210721+HRAshton@users.noreply.github.com>
2025-02-09 12:59:27 +0100

    Rewrite pre_request.py to context manager

--- Matcher/helpers/pre_request.py
+++ Matcher/helpers/pre_request.py
@@ -1,7 +1,7 @@
 import concurrent.futures
 import logging
 from concurrent.futures import Future
-from typing import Callable, TypeVar, Dict
+from typing import Callable, TypeVar, Dict, Generic
 
 from dotenv import load_dotenv
 
@@ -13,45 +13,49 @@ R = TypeVar('R')
 
 USE_MULTIPROCESSING = True  # Use False to prevent PyCharm debug issues
 
-pool = concurrent.futures.ProcessPoolExecutor(max_workers=1)
-results: Dict[int, Callable[[], R]] = {}
-
 logger = logging.getLogger(__name__)
 
 
-def init_pre_request_queue() -> None:
-    global results
-    results.clear()
-    logger.info('Queue was reset.')
+class PreRequestQueue(Generic[A, R]):
+    def __init__(self):
+        self.pool = concurrent.futures.ProcessPoolExecutor(max_workers=1) \
+            if USE_MULTIPROCESSING \
+            else None
+        self.results: Dict[int, Callable[[], R]] = {}
 
+    def __enter__(self):
+        logger.info("Queue initialized.")
+        return self
 
-def pre_request(key: int, func: Callable[[A], R], *args: A) -> None:
-    global results
-    assert key not in results
-    assert len(results) < 2
-    if USE_MULTIPROCESSING:
-        future = _run_in_subprocess(func, *args)
-        results[key] = lambda: future.result()
-    else:
-        logger.warning("Multiprocessing is disabled")
-        results[key] = lambda: func(*args)
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.pool:
+            self.pool.shutdown(wait=True)
+        self.results.clear()
+        logger.info("Queue was reset and resources released.")
 
+    def pre_request(self, key: int, func: Callable[[A], R], *args: A) -> None:
+        assert key not in self.results
+        assert len(self.results) < 2
 
-def get_result(key: int) -> R:
-    global results
-    wav_path, current_duration = results[key]()
-    del results[key]
+        if self.pool:
+            future = self._run_in_subprocess(func, *args)
+            self.results[key] = lambda: future.result()
+        else:
+            logger.warning("Multiprocessing is disabled")
+            self.results[key] = lambda: func(*args)
 
-    return wav_path, current_duration
+    def get_result(self, key: int) -> R:
+        res = self.results[key]()
+        del self.results[key]
+        return res
 
+    def _run_in_subprocess(self, func: Callable[[A], R], *args: A) -> Future[R]:
+        return self.pool.submit(self._run_with_logger, func, *args)
 
-def _run_in_subprocess(func: Callable[[A], R], *args: A) -> Future[R]:
-    return pool.submit(_run_with_logger, func, *args)
-
-
-def _run_with_logger(func: Callable[[A], R], *args: A) -> R:
-    # This function is executed in a subprocess, so we need to set up environment again
-    load_dotenv()
-    Config.initialize_from_env()
-    setup_logging()
-    return func(*args)
+    @staticmethod
+    def _run_with_logger(func: Callable[[A], R], *args: A) -> R:
+        # This function is executed in a subprocess, so we need to set up environment again
+        load_dotenv()
+        Config.initialize_from_env()
+        setup_logging()
+        return func(*args)
--- Matcher/scenes_finder/audio_provider.py
+++ Matcher/scenes_finder/audio_provider.py
@@ -5,8 +5,8 @@ from typing import List, Tuple, Iterator
 import m3u8
 
 from Matcher.config.config import Config
+from Matcher.helpers.pre_request import PreRequestQueue
 from Matcher.scenes_finder.audio_merger import download_and_merge_parts
-from Matcher.helpers.pre_request import pre_request, get_result, init_pre_request_queue
 
 DELETE_TEMP_FILES = True
 
@@ -23,24 +23,24 @@ def get_wav_iter(playlists: List[m3u8.M3U8], opening: bool) -> Iterator[Tuple[st
     if len(playlists) < 2:
         return
 
-    init_pre_request_queue()
-    pre_request(0, _get_wav, playlists[0], opening, 0)
+    with PreRequestQueue() as queue:
+        queue.pre_request(0, _get_wav, playlists[0], opening, 0)
 
-    for i, playlist in enumerate(playlists):
-        if file_path_to_delete is not None and DELETE_TEMP_FILES and os.path.exists(file_path_to_delete):
-            os.remove(file_path_to_delete)
+        for i, playlist in enumerate(playlists):
+            if file_path_to_delete is not None and DELETE_TEMP_FILES and os.path.exists(file_path_to_delete):
+                os.remove(file_path_to_delete)
 
-        wav_path, segments_duration = get_result(i)
-        if i + 1 < len(playlists):
-            pre_request(i + 1, _get_wav, playlists[i + 1], opening, i + 1)
+            wav_path, segments_duration = queue.get_result(i)
+            if i + 1 < len(playlists):
+                queue.pre_request(i + 1, _get_wav, playlists[i + 1], opening, i + 1)
 
-        truncated_duration = min(segments_duration, Config.seconds_to_match)
-        offset = 0 if opening else max(segments_duration - Config.seconds_to_match, 0)
+            truncated_duration = min(segments_duration, Config.seconds_to_match)
+            offset = 0 if opening else max(segments_duration - Config.seconds_to_match, 0)
 
-        truncated_durations_per_episode.append(truncated_duration)
-        file_path_to_delete = wav_path
+            truncated_durations_per_episode.append(truncated_duration)
+            file_path_to_delete = wav_path
 
-        yield wav_path, offset, truncated_duration
+            yield wav_path, offset, truncated_duration
 
 
 def get_truncated_durations() -> List[float]:
