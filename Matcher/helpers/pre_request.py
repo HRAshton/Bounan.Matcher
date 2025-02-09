5bc74e2addbdc2fc40ffbf536424943013e7afbe
HRAshton <12210721+HRAshton@users.noreply.github.com>
2025-02-09 16:56:29 +0100

    (pre_request) fix types, set config in context

--- Matcher/helpers/pre_request.py
+++ Matcher/helpers/pre_request.py
@@ -1,57 +1,74 @@
 import concurrent.futures
 import logging
-from typing import Callable, TypeVar, Dict, Generic
+from typing import Callable, TypeVar, Generic, ParamSpec
 
 from dotenv import load_dotenv
 
 from Matcher.config.config import Config
 from Matcher.matcher_logger import setup_logging
 
-A = TypeVar('A')
-R = TypeVar('R')
+TArgs = ParamSpec('TArgs')
+TResult = TypeVar('TResult')
 
 USE_MULTIPROCESSING = True  # Use False to prevent PyCharm debug issues
 
 logger = logging.getLogger(__name__)
 
 
-class PreRequestQueue(Generic[A, R]):
-    def __init__(self):
-        self.pool = concurrent.futures.ProcessPoolExecutor(max_workers=1) \
-            if USE_MULTIPROCESSING \
-            else None
-        self.results: Dict[int, Callable[[], R]] = {}
+class PreRequestQueue(Generic[TArgs, TResult]):
+    _pool: concurrent.futures.ProcessPoolExecutor | None
+    _results: dict[int, Callable[[], TResult]]
+
+    def __init__(self, config: dict[str, str]):
+        if USE_MULTIPROCESSING:
+            self._pool = concurrent.futures.ProcessPoolExecutor(
+                max_workers=1,
+                initializer=PreRequestQueue._init_worker,
+                initargs=(config,))
+
+        self._results: dict[int, Callable[[], TResult]] = {}
 
     def __enter__(self):
         logger.info("Queue initialized.")
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if self.pool:
-            self.pool.shutdown(wait=True)
-        self.results.clear()
+        if self._pool:
+            self._pool.shutdown(wait=True)
+        self._results.clear()
         logger.info("Queue was reset and resources released.")
 
-    def pre_request(self, key: int, func: Callable[[A], R], config: dict[str, str], *args: A) -> None:
-        assert key not in self.results
-        assert len(self.results) < 2
+    def pre_request(self, key: int,
+                    func: Callable[TArgs, TResult],
+                    *args: TArgs.args,
+                    **kwargs: TArgs.kwargs) -> None:
+        assert key not in self._results
+        assert len(self._results) < 2
+        assert kwargs == {}, "Keyword arguments are not needed for now"
 
-        if self.pool:
-            future = self.pool.submit(self._run_with_logger, func, config, *args)
-            self.results[key] = lambda: future.result()
+        if self._pool:
+            future = self._pool.submit(self._run_with_logger,
+                                       func,
+                                       *args,
+                                       **kwargs)
+            self._results[key] = lambda: future.result()
         else:
             logger.warning("Multiprocessing is disabled")
-            self.results[key] = lambda: func(*args)
+            self._results[key] = lambda: func(*args, **kwargs)
 
-    def get_result(self, key: int) -> R:
-        res = self.results[key]()
-        del self.results[key]
+    def get_result(self, key: int) -> TResult:
+        res = self._results[key]()
+        del self._results[key]
         return res
 
     @staticmethod
-    def _run_with_logger(func: Callable[[A], R], config: dict[str, str], *args: A) -> R:
-        # This function is executed in a subprocess, so we need to set up environment again
+    def _init_worker(config: dict[str, str]) -> None:
         load_dotenv()
         Config.initialize_from_dict(config)
         setup_logging()
-        return func(*args)
+
+    @staticmethod
+    def _run_with_logger(func: Callable[TArgs, TResult],
+                         *args: TArgs.args,
+                         **kwargs: TArgs.kwargs) -> TResult:
+        return func(*args, **kwargs)
