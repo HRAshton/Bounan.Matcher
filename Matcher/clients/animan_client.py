5b2b961348d535bf532af37a4f6d1901d9346eea
HRAshton <12210721+HRAshton@users.noreply.github.com>
2025-02-09 20:48:52 +0100

    Add logging to animan_client.py

--- Matcher/clients/animan_client.py
+++ Matcher/clients/animan_client.py
@@ -1,9 +1,12 @@
+import logging
+
 import boto3
 
 from Common.py.models import VideoKey, Scenes, MatcherResponse, MatcherResultRequest, MatcherResultRequestItem
 from Matcher.config.config import Config
 
 lambda_client = boto3.client('lambda')
+logger = logging.getLogger(__name__)
 
 
 def get_videos_to_match() -> MatcherResponse:
@@ -29,6 +32,7 @@ def update_video_scenes(data: MatcherResultRequest) -> None:
 
 
 def upload_empty_scenes(videos_to_match: list[VideoKey]) -> None:
+    logger.info("Uploading empty scenes for videos: %s", videos_to_match)
     data = [MatcherResultRequestItem(video_key, Scenes(None, None, None))
             for video_key in videos_to_match]
     update_video_scenes(MatcherResultRequest(data))
